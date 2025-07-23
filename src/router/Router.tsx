70bdf80daa34d02f2075dc3588edbe5ddf756093
COUE Vivien <vcoue@externe.viamedis.fr>
2025-07-23 18:41:37 +0200

    Add basename to Router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,7 +4,7 @@ import Home from '../pages/Home';
 import Result from '../pages/Result';
 
 const AppRouter: React.FC = () => (
-  <BrowserRouter>
+  <BrowserRouter basename='react-lvl2-quizz-maker' >
     <Routes>
       <Route path="/result" element={<Result />} />
       <Route path="/home" element={<Home />} />
